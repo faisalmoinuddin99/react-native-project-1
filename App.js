5efad694faf2c6d332fdb001c8dcfbb7d676e645
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-27 10:27:34 +0530

    Todo App (Part 1)

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import Header from "./components/Header";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "Go to gym", key: 1 },
+    { text: "buy coffee", key: 2 },
+    { text: "create an app", key: 3 },
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "wheat",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
